c2e9dc8b496597c18034de40fd00fbf36870a7f2
Victor Wolmeister <wolmeistervictor@gmail.com>
2023-04-19 23:14:25 -0300

    [WEB-EXAMPLE] Setup antd

--- examples/web/src/App.tsx
+++ examples/web/src/App.tsx
@@ -1,31 +1,12 @@
-import './App.css';
+// import './App.css';
 
-import React, { useState } from 'react';
-
-import viteLogo from '../public/vite.svg';
-import reactLogo from './assets/react.svg';
+import { Button } from 'antd';
+import React from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <Button type="primary">Nice</Button>
     </div>
   );
 }
--- examples/web/src/main.tsx
+++ examples/web/src/main.tsx
@@ -1,4 +1,4 @@
-import './index.css';
+import 'antd/dist/reset.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
