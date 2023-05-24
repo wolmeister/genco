5d68dde5b07e69acbc847ac2e169ad6a1a30e8cd
Victor Wolmeister <wolmeistervictor@gmail.com>
2023-05-24 00:25:16 -0300

    [API-EXAMPLE] Fix auth errors using wrong code

--- examples/api/src/modules/auth/auth.errors.ts
+++ examples/api/src/modules/auth/auth.errors.ts
@@ -1,4 +1,4 @@
 import createError from '@fastify/error';
 
-export const NotAuthenticatedError = createError('NOT_AUTHENTICATED', 'Not authenticated', 403);
-export const UnauthorizedError = createError('NOT_UNAUTHORIZED', 'Not authorized', 401);
+export const NotAuthenticatedError = createError('NOT_AUTHENTICATED', 'Not authenticated', 401);
+export const UnauthorizedError = createError('NOT_UNAUTHORIZED', 'Not authorized', 403);
