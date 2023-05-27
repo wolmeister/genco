e7b9ae6ebf18edff2f2f1fdfdb672456b03b7596
Victor Wolmeister <wolmeistervictor@gmail.com>
2023-05-26 23:45:40 -0300

    [WEB-EXAMPLE] Fix antd rendering in jest

--- examples/web/jest.setup.ts
+++ examples/web/jest.setup.ts
@@ -1,1 +1,16 @@
+/// <reference types="@types/jest" />
 import 'whatwg-fetch';
+
+Object.defineProperty(window, 'matchMedia', {
+  value: () => {
+    return {
+      matches: false,
+      addListener: () => {
+        // Do nothing.
+      },
+      removeListener: () => {
+        // Do nothing.
+      },
+    };
+  },
+});
