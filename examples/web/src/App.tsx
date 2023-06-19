f388cf96995ff5094f1f25c34340b9a85b3f137d
Victor Wolmeister <wolmeistervictor@gmail.com>
2023-06-18 21:31:52 -0300

    [WEB-EXAMPLE] Add basic app layout

--- examples/web/src/App.tsx
+++ examples/web/src/App.tsx
@@ -1,3 +1,4 @@
+import { Layout, Typography } from 'antd';
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { RouterProvider } from 'react-router-dom';
@@ -15,7 +16,21 @@ const queryClient = new QueryClient({
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <Layout style={{ minHeight: '100vh', backgroundColor: '#f5f5f5' }}>
+        <Layout.Header
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <Typography.Title level={2} style={{ color: 'white', margin: 0 }}>
+            GenCO
+          </Typography.Title>
+        </Layout.Header>
+        <Layout.Content style={{ padding: '32px 48px' }}>
+          <RouterProvider router={router} />
+        </Layout.Content>
+      </Layout>
     </QueryClientProvider>
   );
 }
